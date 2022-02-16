3386e376d4ed6e700b78a71ab05fbec67a50b558
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-02-16 16:31:59 +0100

    added comments

--- src/utils/fileHasher.ts
+++ src/utils/fileHasher.ts
@@ -1,6 +1,11 @@
 import crypto from 'crypto';
 import fs from 'fs';
 
+/**
+ * This function takes a file path and converts the file into a hash.
+ * @param filePath the path to the file you want to convert as a string.
+ * @returns a hash of the file based on the sha256 algorithm as a string.
+ */
 export function hashFile(filePath: string): string {
 	const targetFile = fs.readFileSync(filePath);
 
@@ -11,7 +16,12 @@ export function hashFile(filePath: string): string {
 	return fileHash;
 }
 
-
+/**
+ * This function checks if an input hash matches with the hash of a file.
+ * @param hash the hash you want to compare the file hash to.
+ * @param filePath the path to the file you want to convert as a string.
+ * @returns true if the hash matches, and false if it does not.
+ */
 export function isHashValid(hash: string, filePath: string): boolean {
 	const fileHash = hashFile(filePath);
 	let isEqual = false;
