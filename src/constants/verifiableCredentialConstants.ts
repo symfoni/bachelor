0dc7e0c64987b9822d30217ad7380cb87e861c3c
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-02-25 11:52:52 +0100

    added comments

--- src/constants/verifiableCredentialConstants.ts
+++ src/constants/verifiableCredentialConstants.ts
@@ -1,9 +1,12 @@
+// Proof formats
 export const PROOF_FORMAT_JWT = 'jwt';
 
+// Schema links
 export const SCHEMA_EMPLOYMENT_CONTRACT = 'https://raw.githubusercontent.com/symfoni/bachelor/dev/schemas/employmentContractSchema.json';
 
 export const SCHEMA_W3_CREDENTIAL = 'https://www.w3.org/2018/credentials/v1';
 
+// Credential types
 export const TYPE_VERIFIABLE_CREDENTIAL = 'VerifiableCredential';
 
 export const TYPE_EMPLOYMENT_CREDENTIAL = 'EmploymentVC';
