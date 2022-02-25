ad5a2ccd80e22404bd52db811c5fa60c3961079a
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-02-25 11:51:28 +0100

    created a constant file for everything credential related

---
+++ src/constants/verifiableCredentialConstants.ts
@@ -0,0 +1,9 @@
+export const PROOF_FORMAT_JWT = 'jwt';
+
+export const SCHEMA_EMPLOYMENT_CONTRACT = 'https://raw.githubusercontent.com/symfoni/bachelor/dev/schemas/employmentContractSchema.json';
+
+export const SCHEMA_W3_CREDENTIAL = 'https://www.w3.org/2018/credentials/v1';
+
+export const TYPE_VERIFIABLE_CREDENTIAL = 'VerifiableCredential';
+
+export const TYPE_EMPLOYMENT_CREDENTIAL = 'EmploymentVC';
