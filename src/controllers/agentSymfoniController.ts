8e43f7d4481e4197a91a785fbd6eb6bf77d144d2
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-02-27 13:01:38 +0100

    added comments, and changed function default parameters

--- src/controllers/agentSymfoniController.ts
+++ src/controllers/agentSymfoniController.ts
@@ -1,12 +1,14 @@
 import { agentSymfoni } from '../veramo/setup';
 
+
 /**
  * createDIDSymfoni creates a did for the symfoni agent
  * @param alias optional name for the did, default is an empty string
  * @param provider optional provider for the did, default is 'did:ethr:rinkeby'
  * @param keyManagementSystem optional key management system for the did, default is 'local'
+ * @returns the created DID
  */
-async function createDIDSymfoni(alias?: string, provider?: string, keyManagementSystem?: string) {
+async function createDIDSymfoni(alias?: string, provider = 'did:ethr:rinkeby', keyManagementSystem = 'local') {
 	const DID = await agentSymfoni.didManagerCreate({
 		alias: alias,
 		provider: provider,
@@ -15,3 +17,4 @@ async function createDIDSymfoni(alias?: string, provider?: string, keyManagement
 
 	return DID;
 }
+
