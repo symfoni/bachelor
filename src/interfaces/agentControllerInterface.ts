09ff115c66ba52013972528e124de6f4e4584500
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-03-09 15:43:34 +0100

    updated the interface

--- src/interfaces/agentControllerInterface.ts
+++ src/interfaces/agentControllerInterface.ts
@@ -1,17 +1,28 @@
+import { DIDResolutionResult, IIdentifier, VerifiableCredential, VerifiablePresentation } from '@veramo/core';
+import { UniqueVerifiableCredential } from '@veramo/data-store';
+
 /**
  * A simple interface for the AgentController class
  */
 export interface IAgentController {
     // TODO: Fix return type any to actually be of type did, 'did[]', etc.
-    getDID(did: string): any
+    getDID(did: string): Promise<IIdentifier | string>
 
-    createDID(alias?: string, provider?: string, keyManagementSystem?: string): any
+    createDID(alias?: string, provider?: string, keyManagementSystem?: string): Promise<IIdentifier | string>
 
-    listAllDIDs(): any
+    listAllDIDs(): Promise<IIdentifier[]>
 
-    listDIDsBasedOnProvider(provider: string): any
+    listDIDsBasedOnProvider(provider: string): Promise<IIdentifier[] | string>
 
-    listDIDsBasedOnAlias(alias: string): any
+    listDIDsBasedOnAlias(alias: string): Promise<IIdentifier[] | string>
 
-    resolveDID(didUrl: string): any
+    resolveDID(didUrl: string): Promise<DIDResolutionResult | string>
+
+    createCredential(credentialData: VerifiableCredential): Promise<VerifiableCredential | string>
+
+    addCredential(credential: VerifiableCredential): Promise<string>
+
+    getAllCredentials(): Promise<UniqueVerifiableCredential[]>
+
+    createPresentation(holder:string,credentials: VerifiableCredential[]): Promise<VerifiablePresentation | string>
 }
