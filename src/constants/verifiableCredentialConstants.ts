839249ac08d2da96316c27fc2c711c057fff0e17
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-03-12 18:12:57 +0100

    added constants related to the business credential

--- src/constants/verifiableCredentialConstants.ts
+++ src/constants/verifiableCredentialConstants.ts
@@ -4,9 +4,13 @@ export const PROOF_FORMAT_JWT = 'jwt';
 // Schema links
 export const SCHEMA_EMPLOYMENT_CONTRACT = 'https://raw.githubusercontent.com/symfoni/bachelor/dev/schemas/employmentContractSchema.json';
 
+export const SCHEMA_BUSINESS_CREDENTIAL = 'https://raw.githubusercontent.com/symfoni/bachelor/dev/schemas/tempJSON/businessSchema.json';
+
 export const SCHEMA_W3_CREDENTIAL = 'https://www.w3.org/2018/credentials/v1';
 
 // Credential types
 export const TYPE_VERIFIABLE_CREDENTIAL = 'VerifiableCredential';
 
 export const TYPE_EMPLOYMENT_CREDENTIAL = 'EmploymentVC';
+
+export const TYPE_BUSINESS_CREDENTIAL = 'BusinessVC';
