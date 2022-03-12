eb941cbec13a33c4ce888d382defa38cfd1191e0
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-03-12 18:37:22 +0100

    added constants related to the person credential

--- src/constants/verifiableCredentialConstants.ts
+++ src/constants/verifiableCredentialConstants.ts
@@ -6,6 +6,8 @@ export const SCHEMA_EMPLOYMENT_CONTRACT = 'https://raw.githubusercontent.com/sym
 
 export const SCHEMA_BUSINESS_CREDENTIAL = 'https://raw.githubusercontent.com/symfoni/bachelor/dev/schemas/tempJSON/businessSchema.json';
 
+export const SCHEMA_PERSON_CREDENTIAL = 'https://github.com/symfoni/bachelor/blob/dev/schemas/tempJSON/personSchema.json';
+
 export const SCHEMA_W3_CREDENTIAL = 'https://www.w3.org/2018/credentials/v1';
 
 // Credential types
@@ -14,3 +16,5 @@ export const TYPE_VERIFIABLE_CREDENTIAL = 'VerifiableCredential';
 export const TYPE_EMPLOYMENT_CREDENTIAL = 'EmploymentVC';
 
 export const TYPE_BUSINESS_CREDENTIAL = 'BusinessVC';
+
+export const TYPE_PERSON_CREDENTIAL = 'PersonVC';
