0c7bf361ff762ddc6e8c31b56df8cbd0d64ac9d7
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-03-24 13:44:40 +0100

    updated the agent controller interface to enforce the returnal of an Error object upon failure

--- src/interfaces/agentControllerInterface.ts
+++ src/interfaces/agentControllerInterface.ts
@@ -6,23 +6,23 @@ import { UniqueVerifiableCredential } from '@veramo/data-store';
  */
 export interface IAgentController {
     // TODO: Fix return type any to actually be of type did, 'did[]', etc.
-    getDID(did: string): Promise<IIdentifier | string>
+    getDID(did: string): Promise<IIdentifier | Error>
 
-    createDID(alias?: string, provider?: string, keyManagementSystem?: string): Promise<IIdentifier | string>
+    createDID(alias?: string, provider?: string, keyManagementSystem?: string): Promise<IIdentifier | Error>
 
     listAllDIDs(): Promise<IIdentifier[]>
 
-    listDIDsBasedOnProvider(provider: string): Promise<IIdentifier[] | string>
+    listDIDsBasedOnProvider(provider: string): Promise<IIdentifier[] | Error>
 
-    listDIDsBasedOnAlias(alias: string): Promise<IIdentifier[] | string>
+    listDIDsBasedOnAlias(alias: string): Promise<IIdentifier[] | Error>
 
-    resolveDID(didUrl: string): Promise<DIDResolutionResult | string>
+    resolveDID(didUrl: string): Promise<DIDResolutionResult | Error>
 
-    createCredential(credentialData: VerifiableCredential): Promise<VerifiableCredential | string>
+    createCredential(credentialData: VerifiableCredential): Promise<VerifiableCredential | Error>
 
     addCredential(credential: VerifiableCredential): Promise<string | Error>
 
     getAllCredentials(): Promise<UniqueVerifiableCredential[]>
 
-    createPresentation(holder:string,credentials: VerifiableCredential[]): Promise<VerifiablePresentation | string>
+    createPresentation(holder:string,credentials: VerifiableCredential[]): Promise<VerifiablePresentation | Error>
 }
