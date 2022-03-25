cb24cddc72b354c70ff9974bbf341b1da4a55730
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-03-25 12:28:00 +0100

    updated the navigation button component so that it can take a custom style as a prop

--- app/components/navigationButton.tsx
+++ app/components/navigationButton.tsx
@@ -1,11 +1,25 @@
-import { ButtonProps, Pressable, Text } from 'react-native';
-import { styles } from '../styles';
+import { GestureResponderEvent, Pressable, StyleProp, Text, ViewStyle } from 'react-native';
+import { buttonStyles } from '../styles';
 
-export default function NavigationButton(props: ButtonProps): JSX.Element{
-	const {onPress, title} = props;
+/**
+ * The interface for the navigation button props.
+ */
+interface INavigationButtonProps {
+    onPress: (event: GestureResponderEvent) => void
+    title: string
+    customStyle?: StyleProp<ViewStyle>
+}
+
+/**
+ * NavigationButton is a customizable navigation button component.
+ * @param props consists of an onPress function, a title, and a custom stylesheet.
+ * @returns a navigation button component.
+ */
+export default function NavigationButton(props: INavigationButtonProps): JSX.Element{
+	const {onPress, title, customStyle = buttonStyles.navigationButtonDefault} = props;
 	return (
-		<Pressable style={styles.navigationButton} onPress={onPress}>
-			<Text style={styles.navigationButtonText}>{title}</Text>
+		<Pressable style={customStyle} onPress={onPress}>
+			<Text style={buttonStyles.navigationButtonText}>{title}</Text>
 		</Pressable>
 	);
 }
