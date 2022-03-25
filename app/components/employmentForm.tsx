98cc83c28161b521d2e1481490a38fc967fad663
magnujg <magnujg@stud.ntnu.no>
2022-03-25 15:43:14 +0100

    Basic form using formik

---
+++ app/components/employmentForm.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Button, TextInput, View } from 'react-native';
+import { styles } from '../styles';
+import { Formik } from 'formik';
+
+const 
+
+export default function EmploymentForm() {
+
+	return (
+    
+		<View style={styles.container}>
+			<Formik
+				initialValues={{ jobTitle: '', hoursOfWork: '', startDate: '' }}
+				onSubmit={(values, actions) => {
+					actions.resetForm();
+					console.log(values);
+				}}
+			>
+				{props => (
+					<View>
+						<TextInput
+							style={styles.input}
+							placeholder='Job title'
+							onChangeText={props.handleChange('jobTitle')}
+							value={props.values.jobTitle}
+						/>
+
+						<TextInput
+							style={styles.input}
+							multiline
+							placeholder='Hours per week'
+							onChangeText={props.handleChange('hoursOfWork')}
+							value={props.values.hoursOfWork}
+							keyboardType='numeric'
+						/>
+
+						<TextInput 
+							style={styles.input}
+							placeholder='01-01-2022'
+							onChangeText={props.handleChange('startDate')}
+							value={props.values.startDate}
+							
+						/>
+            
+						<Button color='maroon' title="Submit" onPress={props.submitForm} /> 
+					</View>
+				)}
+			</Formik>
+		</View>
+    
+	);
+}
