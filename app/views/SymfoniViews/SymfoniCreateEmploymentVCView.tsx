1d0947958d9bd79b0c4cfee0c25602796705f068
magnujg <magnujg@stud.ntnu.no>
2022-03-25 15:44:32 +0100

    added form to the correct page

--- app/views/SymfoniViews/SymfoniCreateEmploymentVCView.tsx
+++ app/views/SymfoniViews/SymfoniCreateEmploymentVCView.tsx
@@ -1,10 +1,12 @@
 import { Text, View } from 'react-native';
 import { styles } from '../../styles';
+import EmploymentForm from '../../Components/employmentForm';
 
 export function SymfoniCreateEmploymentVCView() {
 	return (
 		<View style={styles.container}>
 			<Text>This is where Symfoni creates their employmentVC.</Text>
+			<EmploymentForm />
 		</View>
 	);
 }
