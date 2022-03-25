bb5f11361d7e095a9f1751c894ec169122e1729e
magnujg <magnujg@stud.ntnu.no>
2022-03-25 15:43:52 +0100

    Fixed merge conflict

--- app/styles.ts
+++ app/styles.ts
@@ -32,7 +32,14 @@ export const styles = StyleSheet.create({
 		fontSize: 22,
 		color: '#fff',
 		marginBottom: 15
-	}
+	},
+	input: {
+		borderWidth: 1,
+		borderColor: '#ddd',
+		padding: 10,
+		fontSize: 18,
+		borderRadius: 6,
+	},
 });
 
 // Button styles
