62bac8ef54bae391472f8aa43f9fff4f9db45b02
magnujg <magnujg@stud.ntnu.no>
2022-03-28 13:40:59 +0200

    tested that it works and added some doucmentation

--- app/views/SymfoniViews/SymfoniCreateEmploymentVCView.tsx
+++ app/views/SymfoniViews/SymfoniCreateEmploymentVCView.tsx
@@ -1,8 +1,10 @@
-import { Text, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 import { styles } from '../../styles';
 import EmploymentForm from '../../Components/employmentForm';
+import React, { useState } from 'react';
 
 export function SymfoniCreateEmploymentVCView() {
+
 	return (
 		<View style={styles.container}>
 			<Text>This is where Symfoni creates their employmentVC.</Text>
