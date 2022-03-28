97821d029ce45e321995e52fa7295c3fed57bc92
magnujg <magnujg@stud.ntnu.no>
2022-03-28 11:08:18 +0200

    Added titles and onBlur

--- app/components/employmentForm.tsx
+++ app/components/employmentForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, TextInput, View, Label, Text } from 'react-native';
+import { Button, TextInput, View, Text } from 'react-native';
 import { styles } from '../styles';
 import { Formik } from 'formik';
 import * as yup from 'yup';
@@ -25,30 +25,37 @@ export default function EmploymentForm() {
 			>
 				{props => (
 					<View>
+						<Text>Job Title</Text>
 						<TextInput
 							style={styles.input}
 							placeholder='Job title'
 							onChangeText={props.handleChange('jobTitle')}
 							value={props.values.jobTitle}
+							onBlur={props.handleBlur('title')}
 						/>
 
 						<Text>{props.touched.jobTitle && props.errors.jobTitle}</Text>
 						
+						
+						<Text>Hours per week</Text>
 						<TextInput
 							style={styles.input}
 							placeholder='Hours per week'
 							onChangeText={props.handleChange('hoursOfWork')}
 							value={props.values.hoursOfWork}
 							keyboardType='numeric'
+							onBlur={props.handleBlur('hoursOfWork')}
 						/>
 
 						<Text>{props.touched.hoursOfWork && props.errors.hoursOfWork}</Text>
 
+						<Text>start Date</Text>
 						<TextInput 
 							style={styles.input}
 							placeholder='01-01-2022'
 							onChangeText={props.handleChange('startDate')}
 							value={props.values.startDate}
+							onBlur={props.handleBlur('startDate')}
 							
 						/>
 
