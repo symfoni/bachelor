9527388fdb843f8c99b0ee11a141ef6095eb5ef8
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-03-28 12:42:24 +0200

    made a credential card component

---
+++ app/components/credentialCard.tsx
@@ -0,0 +1,45 @@
+import { View, Text } from 'react-native';
+import { TYPE_BUSINESS_CREDENTIAL, TYPE_EMPLOYMENT_CREDENTIAL, TYPE_PERSON_CREDENTIAL, TYPE_TERMINATION_CREDENTIAL } from '../../src/constants/verifiableCredentialConstants';
+import { styles } from '../styles';
+
+/**
+ * CredentialCard is a component that returns a CredentialCard based on the credential type that it is fed.
+ * @param param0 takes in verifiable credential data.
+ * @returns a credential card for a specific credential type.
+ */
+export function CredentialCard({item}: any) {
+	if (item.verifiableCredential.type[1] === TYPE_EMPLOYMENT_CREDENTIAL) {
+		return (
+			<View style={styles.credentialCard}>
+				<Text style={styles.headingText}>{item.verifiableCredential.type[1]}</Text>
+				<Text style={styles.defaultText}>Job title: {item.verifiableCredential.credentialSubject.employment.employee.jobTitle}</Text>
+				<Text style={styles.defaultText}>Work place: {item.verifiableCredential.credentialSubject.employment.employee.placeofWork}</Text>
+				<Text style={styles.defaultText}>Start date: {item.verifiableCredential.credentialSubject.employment.employee.startDate}</Text>
+			</View>
+		);
+	} else if (item.verifiableCredential.type[1] === TYPE_TERMINATION_CREDENTIAL) {
+		return (
+			<View style={styles.credentialCard}>
+				<Text style={styles.headingText}>{item.verifiableCredential.type[1]}</Text>
+				<Text style={styles.defaultText}>Last day at work: {item.verifiableCredential.credentialSubject.termination.employee.lastDayAtWork}</Text>
+				<Text style={styles.defaultText}>Last payday: {item.verifiableCredential.credentialSubject.termination.employee.lastPayday}</Text>
+				<Text style={styles.defaultText}>Termination status: {item.verifiableCredential.credentialSubject.termination.employee.terminationStatus}</Text>
+			</View>
+		);
+	} else if (item.verifiableCredential.type[1] === TYPE_PERSON_CREDENTIAL) {
+		return (
+			<View style={styles.credentialCard}>
+				<Text style={styles.headingText}>{item.verifiableCredential.type[1]}</Text>
+				<Text style={styles.defaultText}>Name: {item.verifiableCredential.credentialSubject.person.name.firstName}</Text>
+				<Text style={styles.defaultText}>Date of birth: {item.verifiableCredential.credentialSubject.person.dateOfBirth}</Text>
+			</View>
+		);
+	} else if (item.verifiableCredential.type[1] === TYPE_BUSINESS_CREDENTIAL) {
+		return (
+			<View style={styles.credentialCard}>
+				<Text style={styles.headingText}>{item.verifiableCredential.type[1]}</Text>
+			</View>
+		);
+	}
+	return null;
+}
