0debfff4ef86ad1f4ccd4dda6439fb774a708821
magnujg <magnujg@stud.ntnu.no>
2022-03-29 10:23:27 +0200

    added dropdown menu

--- app/components/employmentForm.tsx
+++ app/components/employmentForm.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import { Picker } from '@react-native-picker/picker';
 import { Button, TextInput, View, Text } from 'react-native';
 import { styles, symfoniColor } from '../styles';
 import { Formik } from 'formik';
 import * as yup from 'yup';
+import React, { useState } from 'react';
 
 // A schema for validating the form inputs using the yup library
 const EmploymentSchema = yup.object({
@@ -11,10 +12,14 @@ const EmploymentSchema = yup.object({
 	startDate: yup.date().required()
 });
 
+
+
 // TODO: Add a way to navigate to the QR page when submitting the form
 // TODO: Implement a way to pass the props to the proper endpoint
 
 export default function EmploymentForm() {
+
+	const [selectedEmploymentState, setSelectedEmploymentState] = useState('fullTime');
 
 	return (
     
@@ -24,7 +29,8 @@ export default function EmploymentForm() {
 		 */
 		<View style={styles.container}>
 			<Formik
-				initialValues={{ jobTitle: '', hoursOfWork: '', startDate: '' }}
+				initialValues={{ jobTitle: '', placeOfWork: '', hoursOfWork: '', startDate: '', endDate: '',  employmentType: '', partTimePercentage: '', frequency: '', amount: '',
+					currency: '', trialStartDate: '', trialEndDate: '', rightForPension: false, nonCompeteClause: false, requirementToWorkOversees: false, employmentStatus: ''}}
 				validationSchema={EmploymentSchema}
 				onSubmit={(values, actions) => {
 					actions.resetForm();		
@@ -42,6 +48,8 @@ export default function EmploymentForm() {
 						/>
 
 						<Text>{props.touched.jobTitle && props.errors.jobTitle}</Text>
+
+						
 						
 						
 						<Text>Hours per week</Text>
@@ -65,6 +73,35 @@ export default function EmploymentForm() {
 							onBlur={props.handleBlur('startDate')}
 							
 						/>
+
+						<Text>Trial period</Text>
+						<TextInput 
+							style={styles.input}
+							placeholder='Start: MM-DD-YYYY'
+							onChangeText={props.handleChange('trialStartDate')}
+							value={props.values.startDate}
+							onBlur={props.handleBlur('trialStartDate')}
+							
+						/>
+						<TextInput 
+							style={styles.input}
+							placeholder='End: MM-DD-YYYY'
+							onChangeText={props.handleChange('trialEndDate')}
+							value={props.values.startDate}
+							onBlur={props.handleBlur('trialEndDate')}
+						/>
+
+						<Text>Employment status</Text>
+						<Picker
+							selectedValue={selectedEmploymentState}
+							onValueChange={(itemValue) => setSelectedEmploymentState(itemValue)}
+						>
+							<Picker.Item label='full time' value='full time'></Picker.Item>
+							<Picker.Item label='part time' value='part time'></Picker.Item>
+							<Picker.Item label='freelance' value='freelance'></Picker.Item>
+						</Picker>
+							
+						
 
 						<Text>{props.touched.startDate && props.errors.startDate}</Text>
             
