0e80ad7c2cf5a2503f07ea328093eabe02b6ac9b
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-03-29 16:17:01 +0200

    commenting

--- app/views/UserViews/UserVCDetailView.tsx
+++ app/views/UserViews/UserVCDetailView.tsx
@@ -3,9 +3,11 @@ import { View } from 'react-native';
 import { CredentialDetail } from '../../components/credentialDetail';
 import { styles } from '../../styles';
 
-
-
-
+/**
+ * UserVCDetailView is a view that renders the details of a selcted VC.
+ * @param param0 a verifiable credential object.
+ * @returns the details within the verifiable credential that was passed from the parameter.
+ */
 export function UserVCDetailView({route}:any) {
 	const {item} = route.params;
 
