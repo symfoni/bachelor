c76843c6baf1e1bed0dab924860f18e847717345
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-03-30 16:15:41 +0200

    made a connection button

--- app/views/NAVViews/NAVRecieveVPView.tsx
+++ app/views/NAVViews/NAVRecieveVPView.tsx
@@ -1,10 +1,16 @@
-import { Text, View } from 'react-native';
+import { useWalletConnect } from '@walletconnect/react-native-dapp';
+import * as React from 'react';
+import { Button, View } from 'react-native';
 import { styles } from '../../styles';
 
 export function NAVRecieveVPView() {
-	return (
-		<View style={styles.container}>
-			<Text>This is where NAV recieves a VP.</Text>
-		</View>
-	);
+	const connector = useWalletConnect();
+	if (!connector.connected) {
+		return (
+			<View style={styles.container}>
+				<Button title="Connect" onPress={() => connector.connect()} />
+			</View>
+		);
+	}
+	return <Button title="Kill Session" onPress={() => connector.killSession()} />;
 }
