267fcc36854b96af442031cfd9123e2f7ebf5f3f
magnujg <magnujg@stud.ntnu.no>
2022-03-31 13:28:54 +0200

    hotfix components import

--- app/views/StateViews/StateCreatePersonVCView.tsx
+++ app/views/StateViews/StateCreatePersonVCView.tsx
@@ -2,7 +2,7 @@
 import { styles } from '../../styles';
 import React from 'react';
 import { View } from 'react-native';
-import StateForm from '../../Components/stateForm';
+import StateForm from '../../components/stateForm';
 
 // A screen with the state form. Takes you to the next page if the form is validated.
 export function StateCreatePersonVCView() {
