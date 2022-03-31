812aacf70eb6106bcc8f92a60c4c3416a6366175
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-03-31 16:15:51 +0200

    updated text and style

--- app/views/UserViews/UserListVCView.tsx
+++ app/views/UserViews/UserListVCView.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { FlatList, View, ActivityIndicator, Platform } from 'react-native';
+import { FlatList, View, ActivityIndicator, Platform, Text } from 'react-native';
 import { CredentialCard } from '../../components/credentialCard';
 import { styles } from '../../styles';
 
@@ -13,7 +13,7 @@ const userCredentialEndpoint = Platform.OS === 'android' ? 'http://localhost:606
  * UserListVCView is a view that fetches all locally stored credentials and displays them for the user.
  * @returns a view with a list of credentials owned by the user.
  */
-export default function UserListVCView({navigation}: any) {
+export default function UserListVCView({ navigation }: any) {
 	const [isLoading, setLoading] = useState(true);
 	const [data, setData] = useState([]);
 
@@ -22,7 +22,7 @@ export default function UserListVCView({navigation}: any) {
 			const response = await fetch(userCredentialEndpoint);
 			const json = await response.json();
 			setData(json.listOfCredentials);
-		} catch (error) {        
+		} catch (error) {
 			console.error(error);
 		} finally {
 			setLoading(false);
@@ -35,12 +35,13 @@ export default function UserListVCView({navigation}: any) {
 
 	return (
 		<View style={styles.container}>
-			{isLoading ? <ActivityIndicator/> : (
+			<Text style={styles.headingTextBlack}>Here are all of your credentials!</Text>
+			{isLoading ? <ActivityIndicator /> : (
 				<FlatList
 					data={data}
 					keyExtractor={({ id }) => id}
 					// eslint-disable-next-line @typescript-eslint/no-explicit-any
-					renderItem={({ item }: any ) => (
+					renderItem={({ item }: any) => (
 						// TODO: Fix 'each child in list should have a unique key' on mobile build.
 						<CredentialCard key={item.hash} item={item} navigation={navigation}></CredentialCard>
 					)}
