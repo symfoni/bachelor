f34a1ed8f20bcbfe4e3d0f36ceaa5478048466ac
magnujg <magnujg@stud.ntnu.no>
2022-03-31 13:13:00 +0200

    Renamed some variables, fixed some imports

--- app/components/stateForm.tsx
+++ app/components/stateForm.tsx
@@ -5,7 +5,7 @@ import { buttonStyles, formStyles, styles } from '../styles';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 
-// Enum for validating preset Social Security Numbers.
+// Enum for validating predefined Social Security Numbers.
 enum SocialSecurityNumber {
 	SSN_1 = '1234',
 	SSN_2 = '5678',
@@ -14,27 +14,27 @@ enum SocialSecurityNumber {
 
 // Validation schema using the yup library.
 const ValidationSchema = yup.object({
-	lastName: yup.mixed<SocialSecurityNumber>().oneOf(Object.values(SocialSecurityNumber), 'Must be a valid Social Security Number').required()
+	socialSecurityNumber: yup.mixed<SocialSecurityNumber>().oneOf(Object.values(SocialSecurityNumber), 'Must be a valid Social Security Number').required()
 	
 });
 
 
 /**
  * StateForm is a form component for writing in your social security number. 
- * This is just mocked data, as there is no database only an enum with approved SSN.
+ * This is just mocked data, as there is no database, only an enum with approved SSN.
  * If the form validation passes it takes you to a new screen.
  * @returns 
  */
 export default function StateForm({ screenName }: any) {
 
-	// Need to use useNavigation for handeling components not in the screen stack.
+	// Need to use useNavigation for handling components not in the screen stack.
 	const navigation = useNavigation();
 
 	return(
 		<View style={styles.container}>
 			<Formik
 				initialValues={{
-					lastName: ''
+					socialSecurityNumber: ''
 				}}
 				validationSchema={ValidationSchema}
 				onSubmit={() => { 
@@ -43,17 +43,17 @@ export default function StateForm({ screenName }: any) {
 			>
 				{props => (
 					<View>
-						<Text style={formStyles.textLabel}>Last Name</Text>
+						<Text style={formStyles.textLabel}>Social Security Number</Text>
                     
 						<TextInput
 							style={formStyles.textInput}
-							placeholder='Last Name'
-							onChangeText={props.handleChange('lastName')}
-							value={props.values.lastName}
-							onBlur={props.handleBlur('lastName')} 
+							placeholder='Social Security Number'
+							onChangeText={props.handleChange('socialSecurityNumber')}
+							value={props.values.socialSecurityNumber}
+							onBlur={props.handleBlur('socialSecurityNumber')} 
 						/>
 
-						<Text>{props.touched.lastName && props.errors.lastName}</Text>
+						<Text>{props.touched.socialSecurityNumber && props.errors.socialSecurityNumber}</Text>
 
 						<Pressable style={buttonStyles.submitButtonFormSymfoni} onPress={props.submitForm}>
 							<Text style={buttonStyles.submitButtonText}>Submit</Text>
--- app/views/StateViews/StateCreatePersonVCView.tsx
+++ app/views/StateViews/StateCreatePersonVCView.tsx
@@ -1,10 +1,8 @@
 
 import { styles } from '../../styles';
-import React, { useEffect, useState } from 'react';
-import stateForm from '../../Components/stateForm';
-import { Text, FlatList, View, ActivityIndicator, Platform } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 import StateForm from '../../Components/stateForm';
-//import { Button } from 'react-native-elements/dist/buttons/Button';
 
 // A screen with the state form. Takes you to the next page if the form is validated.
 export function StateCreatePersonVCView() {
