fdb61850557898f0b8cc078e608246e89040302c
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-04-04 12:25:15 +0200

    made database operation functions

---
+++ src/firestore/operations.ts
@@ -0,0 +1,83 @@
+import { employmentVC } from '../types/employmentVCType';
+import { terminationVC } from '../types/terminationVCType';
+import { db } from './setup';
+
+// the name of the employment contract collection in the database
+const EMPLOYMENT_COLLECTION = 'employment-contracts';
+
+// the name of the termination contract collection in the database
+const TERMINATION_COLLECTION = 'termination-contracts';
+
+/**
+ * dbAddEmploymentContract takes an id and data related to an employment contract, and stores it in a database.
+ * @param id this will be the id of the document.
+ * @param employmentData this will be the data inside the document.
+ * @returns an error if the database insertion failed.
+ */
+export async function dbAddEmploymentContract(id: string, employmentData: employmentVC['credentialSubject']): Promise<void | Error> {
+	try {
+		const docRef = db.collection(EMPLOYMENT_COLLECTION).doc(id);
+		await docRef.set(employmentData);
+	} catch (error) {
+		console.error('database insertion failed', error);
+		return new Error('database insertion failed');
+	}  
+}
+
+/**
+ * dbAddTerminationContract takes an id and data related to a termination contract, and stores it in a database.
+ * @param id this will be the id of the document.
+ * @param terminationData this will be the data inside the document.
+ * @returns an error if the database insertion failed.
+ */
+export async function dbAddTerminationContract(id: string, terminationData: terminationVC['credentialSubject']): Promise<void | Error> {
+	try {
+		const docRef = db.collection(TERMINATION_COLLECTION).doc(id);
+		await docRef.set(terminationData);
+	} catch (error) {
+		console.error('database insertion failed', error);
+		return new Error('database insertion failed');
+	}
+}
+
+/**
+ * dbGetEmploymentContract retrieves the employment contract document data related to the id parameter.
+ * @param id the id of the document you want to retrieve.
+ * @returns the document data.
+ */
+export async function dbGetEmploymentContract(id:string): Promise<FirebaseFirestore.DocumentData | Error | undefined> {
+	try {
+		const employmentContractRef = db.collection(EMPLOYMENT_COLLECTION).doc(id);
+		const doc = await employmentContractRef.get();
+		if (!doc.exists) {
+			console.log('No such document!');
+		} else {
+			console.log('Document data:', doc.data());
+			return doc.data();
+		}
+	} catch (error) {
+		console.error();
+		return new Error('unable to retrieve document from database');
+	}
+}
+
+/**
+ * dbGetTerminationContract retrieves the termination contract document data related to the id parameter.
+ * @param id the id of the document you want to retrieve.
+ * @returns the document data.
+ */
+export async function dbGetTerminationContract(id:string) {
+	try {
+		const terminationContractRef = db.collection(TERMINATION_COLLECTION).doc(id);
+		const doc = await terminationContractRef.get();
+		if (!doc.exists) {
+			console.log('No such document!');
+		} else {
+			console.log('Document data:', doc.data());
+			return doc.data();
+		}
+	} catch (error) {
+		console.error();
+		return new Error('unable to retrieve document from database');
+	}
+}
