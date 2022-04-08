2dd0fc44021f9be65aa1d2459f367f37f55831f5
magnujg <magnujg@stud.ntnu.no>
2022-04-08 14:11:32 +0200

    added constants for messaging path

---
+++ src/constants/didConstants.ts
@@ -0,0 +1,5 @@
+export const navMessagingServiceEndpoint = 'http://localhost:6060/nav/messaging';
+
+export const symfoniMessagingServiceEndpoint = 'http://localhost:6060/symfoni/messaging';
+
+export const userMessagingServiceEndpoint = 'http://localhost:6060/user/messaging';
