a5fa1e49a134bc614eccbe0f7a47b50bcfa64eef
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-04-11 20:26:14 +0200

    added commenting, refactored api url

--- app/views/NAVViews/NAVRecieveVPView.tsx
+++ app/views/NAVViews/NAVRecieveVPView.tsx
@@ -1,24 +1,29 @@
 import QRCode from 'react-native-qrcode-svg';
-import { ActivityIndicator, Platform, Text, View } from 'react-native';
+import { ActivityIndicator, Text, View } from 'react-native';
 import { styles } from '../../styles';
 import { useEffect, useState } from 'react';
 import { TYPE_EMPLOYMENT_CREDENTIAL, TYPE_PERSON_CREDENTIAL, TYPE_TERMINATION_CREDENTIAL } from '../../../src/constants/verifiableCredentialConstants';
+import { NAV_GET_MAIN_IDENTIFIER_URL } from '../../apiConstants';
 
-const GET_MAIN_IDENTIFIER_URL = Platform.OS === 'android' ? 'http://localhost:6060/nav/mainIdentifier' : 'http://localhost:6060/nav/mainIdentifier';
-
+/**
+ * NAVRecieveVPView is responsible for presenting a QR-code with the neccessary information needed
+ * to send a VP to NAV.
+ * @returns a view containing a QR-code with navs address and the required VCs for unemployment benefits.
+ */
 export function NAVRecieveVPView() {
 	const [isLoading, setLoading] = useState(true);
 	const [mainIdentifier, setMainIdentifier] = useState([]);
+	
 	// TODO: Could make the credentials array dynamic, by pushing each one to an array based on input into a form.
 	const QRCodeData = {
 		did: mainIdentifier,
 		credentials: [TYPE_PERSON_CREDENTIAL, TYPE_EMPLOYMENT_CREDENTIAL, TYPE_TERMINATION_CREDENTIAL]
 	};
 
-	// Retrieves NAVs main identifier
+	// Retrieves NAVs main identifier. This is important for the end user to know where they'll send their VP.
 	const getMainIdentifier = async () => {
 		try {
-			const response = await fetch(GET_MAIN_IDENTIFIER_URL);
+			const response = await fetch(NAV_GET_MAIN_IDENTIFIER_URL);
 			const json = await response.json();
 			setMainIdentifier(json.mainIdentifier.did);
 		} catch (error) {
