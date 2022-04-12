f411cabcab2e480cbd0906617f090e927b2c1ee1
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-04-12 12:38:43 +0200

    made a schema validation function that fetches schema from a url

--- src/utils/schemaValidation.ts
+++ src/utils/schemaValidation.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import { Schema, Validator } from 'jsonschema';
+import axios from 'axios';
 
 /**
  * validateSchema is a function that reads a schema from a file and validates it against a JSON-object.
@@ -28,5 +29,34 @@ export function validateSchema(schemaFilePath: string, jsonObject: object): bool
 	} catch (error) {
 		// TODO: Find a better return statement
 		return false;
+	}
+}
+
+/**
+ * validateSchemaWithURL does a fetch request do a url where the schema exists and checks if it validates the object
+ * that was passed through the parameters.
+ * @param schemaURL the url where the schema is.
+ * @param jsonObject the object you want to validate the schema against.
+ * @returns a boolean stating whether the json object was valid against the schema or not.
+ */
+export async function validateSchemaWithURL(schemaURL: string, jsonObject: object): Promise<boolean | Error> {
+	try {
+		const validator = new Validator();
+
+		// fetch schema from url
+		const res = await axios.get(schemaURL);
+		const schema: Schema = res.data;
+		
+		const result = validator.validate(jsonObject, schema);
+
+		if (!result.valid) {
+			return false;
+		}
+		
+		return true;
+
+	} catch (error) {
+		console.error(error);
+		return new Error('unable to validate against schema');
 	}
 }
