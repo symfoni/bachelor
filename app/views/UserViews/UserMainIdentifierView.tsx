f0f5759b5b28b47894723bf537ce08525fe0cc0f
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-04-13 15:20:59 +0200

    created a view where you can see the users main identifier

---
+++ app/views/UserViews/UserMainIdentifierView.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, ActivityIndicator } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
+import { USER_GET_MAIN_IDENTIFIER } from '../../apiConstants';
+import { IMainIdentifier } from '../../interfaces/IMainIdentifier.interface';
+import { styles } from '../../styles';
+  
+/**
+ * UserMainIdentifierView is a view that shows the users main identifier as a QRCode and in plain text.
+ * @returns a view with the users main identifier on display.
+ */
+export default function UserMainIdentifierView() {
+	const [isLoading, setLoading] = useState(true);
+	const [mainIdentifier, setMainIdentifier] = useState<IMainIdentifier>();
+
+	const getMainIdentifier = async () => {
+		try {
+			const result = await fetch(USER_GET_MAIN_IDENTIFIER);
+			const jsonResult = await result.json();
+
+			setMainIdentifier(jsonResult.mainIdentifier);
+		} catch (error) {
+			console.error(error);   
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	useEffect(()=>{
+		getMainIdentifier();
+	},[]);
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.headingTextBlack}>This is your did address!</Text>
+			{isLoading ? <ActivityIndicator></ActivityIndicator> : (
+				<>
+					{typeof mainIdentifier === 'undefined' ? <Text>Something went wrong</Text> : (
+						<>
+							<QRCode value={mainIdentifier.did} size={200}></QRCode>
+							<Text>In plain text:</Text><Text style={{ marginHorizontal: 50 }}>{mainIdentifier.did}</Text>
+						</>
+					)}
+				</>
+			)}
+		</View>
+	);
+}
