55059b071cba0cc185d4c083e68b34fc39e4b441
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-04-13 10:37:00 +0200

    created a function that computes the number of years between two dates

---
+++ src/utils/dateUtils.ts
@@ -0,0 +1,16 @@
+import moment from 'moment';
+
+/**
+ * computeYearsBetweenDates computes the year difference between two dates.
+ * @param date1 the first date as a string
+ * @param date2 the second date as a string
+ * @returns the absolute value of the year difference.
+ */
+export function computeYearsBetweenDates(date1: string, date2: string): number {
+	const momentDate1 = moment(date1);
+	const momentDate2 = moment(date2);
+
+	const yearDiff = momentDate1.diff(momentDate2,'years');
+    
+	return Math.abs(yearDiff);
+}
