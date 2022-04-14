478c19137bd5a514117f7e98b29930e92ad58f75
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-04-14 12:33:09 +0200

    created a view where the user can the see their messages

--- app/views/UserViews/UserSeeMessagesView.tsx
+++ app/views/UserViews/UserSeeMessagesView.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, ActivityIndicator } from 'react-native';
+import { USER_GET_ALL_MESSAGES } from '../../api.constants.';
+import { MessageCard } from '../../components/messageCard';
 import { styles } from '../../styles';
 
 export default function UserSeeMessagesView(){
+	const [isLoading, setLoading] = useState<boolean>(true);
+	const [data, setData] = useState([]);
+
+	const getMessages = async () => {
+		try {
+			const rawdata = await fetch(USER_GET_ALL_MESSAGES);
+			const json = await rawdata.json();
+			setData(json.messages);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	useEffect(()=>{
+		getMessages();
+	},[]);
+
 	return (
 		<View style={styles.container}>
 			<Text style={styles.headingTextBlack}>Messages</Text>
+			{isLoading ? <ActivityIndicator /> : (
+				<>
+					{data.length === 0 ? <Text>No messages found</Text> : (
+						<FlatList
+							data={data}
+							keyExtractor={({ id }) => id}
+							// eslint-disable-next-line @typescript-eslint/no-explicit-any
+							renderItem={({ item }: any) => (
+								<MessageCard item={item}></MessageCard>
+							)}
+						/>
+					)}
+				</>
+			)}
 		</View>
 	);
 }
