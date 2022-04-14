ba6677ee7574b79875a11f31abd00c6e2ea9ab0a
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-04-14 12:30:24 +0200

    created a message card component

---
+++ app/components/messageCard.tsx
@@ -0,0 +1,15 @@
+import { IMessage } from '@veramo/core';
+import React from 'react';
+import { Pressable, Text } from 'react-native';
+import { styles } from '../styles';
+
+// TODO: Make navigation go to a view where you can see the full message
+// should be able to delete message, add cred to database if cred available etc..
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function MessageCard({item, navigation}: {item: IMessage, navigation?: any}){
+	return (
+		<Pressable style={styles.credentialCard}>
+			<Text>{item.type}</Text>
+		</Pressable>
+	);
+}
