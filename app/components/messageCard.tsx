a791df38863d401ddd5a69ff47b560d90bd660f6
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-04-14 12:52:34 +0200

    added onPress that takes you to the message detail view

--- app/components/messageCard.tsx
+++ app/components/messageCard.tsx
@@ -6,10 +6,15 @@ import { styles } from '../styles';
 // TODO: Make navigation go to a view where you can see the full message
 // should be able to delete message, add cred to database if cred available etc..
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function MessageCard({item, navigation}: {item: IMessage, navigation?: any}){
+export function MessageCard({item, navigation}: {item: IMessage, navigation: any}){
 	return (
-		<Pressable style={styles.credentialCard}>
-			<Text>{item.type}</Text>
+		<Pressable 
+			style={styles.credentialCard}
+			onPress={()=> navigation.navigate('UserMessageDetail', {
+				item: item
+			})}
+		>
+			<Text style={styles.headingText}>{item.type}</Text>
 		</Pressable>
 	);
 }
