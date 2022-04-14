885c4349cc64826bb80e712a3a4fec8ecc15a5f6
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-04-14 14:37:45 +0200

    created a message detail view

--- app/views/UserViews/UserMessageDetailView.tsx
+++ app/views/UserViews/UserMessageDetailView.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import { MessageDetail } from '../../components/messageDetail';
 import { styles } from '../../styles';
 
-export function UserMessageDetailView(){
+/**
+ * UserMessageDetailView is the view where the user can view details within a message.
+ * @param param0 takes an object with a navigator and a route that contains the message details.
+ * @returns a view with message details.
+ */
+export function UserMessageDetailView({navigation, route}: any){
+	
+	const {item} = route.params;
+
 	return (
 		<View style={styles.container}>
-			<Text>Hello</Text>
+			<MessageDetail navigation={navigation} item={item}></MessageDetail>
 		</View>
 	);
 }
