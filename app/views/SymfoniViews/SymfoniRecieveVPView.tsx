22cb90b0e2d82b76f1e8e5fcb17cbc76836cfd12
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-04-14 09:38:42 +0200

    created the view where symfoni shows their main did address and which VC they need

---
+++ app/views/SymfoniViews/SymfoniRecieveVPView.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, ActivityIndicator } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
+import { TYPE_PERSON_CREDENTIAL } from '../../../src/constants/verifiableCredentialConstants';
+import { SYMFONI_GET_MAIN_IDENTIFIER_URL } from '../../apiConstants';
+import { IMainIdentifier } from '../../interfaces/IMainIdentifier.interface';
+import { styles } from '../../styles';
+
+export default function SymfoniRecieveVPView(){
+	const [isLoading, setLoading] = useState<boolean>(true);
+	const [mainIdentifier, setMainIdentifier] = useState<IMainIdentifier>();
+	
+	// TODO: Could make the credentials array dynamic, by pushing each one to an array based on input into a form.
+	const QRCodeData = {
+		did: mainIdentifier,
+		credentials: [TYPE_PERSON_CREDENTIAL]
+	};
+
+	// Retrieves NAVs main identifier. This is important for the end user to know where they'll send their VP.
+	const getMainIdentifier = async () => {
+		try {
+			const response = await fetch(SYMFONI_GET_MAIN_IDENTIFIER_URL);
+			const json = await response.json();
+			setMainIdentifier(json.mainIdentifier.did);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		getMainIdentifier();
+	}, []);
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.headingTextBlack}>Scan the QRCode to send a VP!</Text>
+			{ isLoading ? <ActivityIndicator></ActivityIndicator> : (
+				<QRCode value={JSON.stringify(QRCodeData)} size={200}></QRCode>
+			)}
+		</View> 
+	);
+}
