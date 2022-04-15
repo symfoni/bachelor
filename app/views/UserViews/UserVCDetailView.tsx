9d5e7efaf20cbb4cdf184b69ba2fe906e622a4ab
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-04-15 12:39:44 +0200

    updated the vc detail view with the functionality to delete a credential

--- app/views/UserViews/UserVCDetailView.tsx
+++ app/views/UserViews/UserVCDetailView.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Alert, Button, View } from 'react-native';
+import { USER_CREDENTIAL_URL } from '../../api.constants.';
 import { CredentialDetail } from '../../components/credentialDetail';
 import { styles } from '../../styles';
 
@@ -9,13 +10,41 @@ import { styles } from '../../styles';
  * @returns the details within the verifiable credential that was passed from the parameter.
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function UserVCDetailView({route}:any) {
+export function UserVCDetailView({route, navigation}:any) {
 	const {item} = route.params;
+
+	// deletes a credential
+	const deleteCredential = async () => {
+		//
+		await fetch(USER_CREDENTIAL_URL + '/' + `${item.hash}`, {
+			method: 'DELETE'
+		});
+		navigation.navigate('UserListVCs');
+		return Alert.alert('Deleted', 'The credential was deleted');
+	};
+
+	// displays an alert before you actually delete the credential
+	const deleteAlert = () => {
+		return (
+			Alert.alert('Delete', 'Are you sure you want to delete this credential?', [
+				{
+					text: 'cancel',
+					onPress: () => {return;}
+				},
+				{
+					text: 'delete',
+					onPress: deleteCredential
+				}
+			])
+		);
+	};
 
 	return (  
 		<View style={styles.container}>
-			<CredentialDetail item={item}></CredentialDetail>
-		</View>      
-	
+			<CredentialDetail item={item}></CredentialDetail>	
+			<View style={{margin: 10}}>
+				<Button title='Delete' onPress={deleteAlert}></Button>
+			</View>
+		</View>  
 	);
 }
