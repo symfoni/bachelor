a828a6b6b2c4eb8ef79a49988a4150371fe6b751
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-04-16 11:32:08 +0200

    created a user login page

--- app/views/StateViews/StateCreatePersonVCView.tsx
+++ app/views/StateViews/StateCreatePersonVCView.tsx
@@ -1,15 +1,59 @@
 
-import { styles } from '../../styles';
+import { buttonStyles, styles } from '../../styles';
 import React from 'react';
-import { View } from 'react-native';
-import StateForm from '../../components/stateForm';
+import { TextInput, View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
+import { STATE_PERSON_URL } from '../../api.constants.';
 
-// A screen with the state form. Takes you to the next page if the form is validated.
-export function StateCreatePersonVCView() {
-	
+/**
+ * StateUserLoginView is a view where a user can log into their state account with their SSN.
+ * @param param0 navigatior.
+ * @returns a view where the user can log into their state account using their SSN.
+ */
+export function StateUserLoginView({navigation}: any) {
+	const [text, setText] = React.useState('');
+	const [invalid, setInvalid] = React.useState(false);
+
+	// fetch person data from the database
+	const getPersonData = async () => {
+		try {
+			const rawdata = await fetch(STATE_PERSON_URL + text);
+			const json = await rawdata.json();
+			
+			if (rawdata.status === 200) {
+				navigation.navigate('StateUserPage', {
+					item: json
+				});
+				return;
+			}
+
+			setInvalid(true);
+
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
 	return (
 		<View style={styles.container}>
-			<StateForm screenName='RequestCredential'/>
+			<TextInput 
+				onChangeText={text => setText(text)}
+				onChange={()=>{setInvalid(false);}}
+				keyboardType='numeric' 
+				style={styles.textInputField} 
+				placeholder='Social security number'
+			>
+
+			</TextInput>
+			<Button 
+				buttonStyle={buttonStyles.stateLoginButton}
+				title={'Login'} 
+				onPress={getPersonData}
+			>
+
+			</Button>
+
+			{invalid ? <Text>Not a valid SSN, please try again</Text> : <></>}
 		</View>
 	);
 }
