38fc83cf103811a1d5246201a053109488362a5d
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-04-20 12:01:14 +0200

    message card now displays the did that sent the message by looking into the verifiable registry

--- app/components/messageCard.tsx
+++ app/components/messageCard.tsx
@@ -1,12 +1,35 @@
 import { IMessage } from '@veramo/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 import { styles } from '../styles';
+import verifiableRegistry from '../../verifiableRegistry.json';
+
+interface IVerifiableRegistry {
+	nav: string,
+	state: string,
+	symfoni: string
+}
 
 // TODO: Make navigation go to a view where you can see the full message
 // should be able to delete message, add cred to database if cred available etc..
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function MessageCard({item, navigation}: {item: IMessage, navigation: any}){
+	const [verifiedIssuer, setVerifiedIssuer] = useState<string>('unknown');
+
+	// check if the did that sent the message is a verified issuer.
+	const checkForVerifiedIssuer = ()=>{
+		for (const key in verifiableRegistry) {
+			if (verifiableRegistry[key as keyof IVerifiableRegistry] === item.from) {
+				setVerifiedIssuer(key);
+			}
+		}
+		return;
+	};
+
+	useEffect(()=>{
+		checkForVerifiedIssuer();
+	},[]);
+
 	return (
 		<TouchableOpacity 
 			style={styles.credentialCard}
@@ -14,7 +37,8 @@ export function MessageCard({item, navigation}: {item: IMessage, navigation: any
 				item: item
 			})}
 		>
-			<Text style={styles.headingText}>{item.type}</Text>
+			<Text style={styles.headingText}>Subject: {item.type}</Text>
+			<Text style={styles.defaultText}>From: {verifiedIssuer}</Text>
 		</TouchableOpacity>
 	);
 }
