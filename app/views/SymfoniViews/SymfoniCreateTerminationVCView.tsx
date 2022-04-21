27edc469a4cf482f7d4e53193e5e4610374434f9
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-04-21 11:19:50 +0200

    removed unused imports, imported react, and added comments

--- app/views/SymfoniViews/SymfoniCreateTerminationVCView.tsx
+++ app/views/SymfoniViews/SymfoniCreateTerminationVCView.tsx
@@ -1,8 +1,13 @@
 
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import { styles } from '../../styles';
 import TerminationForm from '../../components/terminationForm';
+import React from 'react';
 
+/**
+ * SymfoniCreateTerminationVCView is a view where the employer can terminate an employee and add the termination data to a database by filling out a form.
+ * @returns a view with a termination contract form.
+ */
 export function SymfoniCreateTerminationVCView() {
 	return (
 		<View style={styles.container}>
