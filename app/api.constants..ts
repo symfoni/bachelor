c327fb7abe1946f351f9d22ece5c9738fec77dc8
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-04-21 11:26:58 +0200

    refactored add employment contract to db URL

--- app/api.constants..ts
+++ app/api.constants..ts
@@ -28,6 +28,7 @@ export const NAV_GET_MAIN_IDENTIFIER_URL = `http://${host}:${PORT}/nav/mainIdent
 
 // symfoni
 export const SYMFONI_GET_MAIN_IDENTIFIER_URL = `http://${host}:${PORT}/symfoni/mainIdentifier`;
+export const SYMFONI_ADD_EMPLOYMENT_CONTRACT_TO_DB_URL = `http://${host}:${PORT}/symfoni/employmentContract`;
 
 // state
 export const STATE_PERSON_URL = `http://${host}:${PORT}/state/person/`;
