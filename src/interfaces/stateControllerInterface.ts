c09a7808481ec40f4a05b125419ccc8f50282ee9
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-04-28 20:02:16 +0200

    added comments

--- src/interfaces/stateControllerInterface.ts
+++ src/interfaces/stateControllerInterface.ts
@@ -1,7 +1,9 @@
 import { VerifiableCredential } from '@veramo/core';
 import { businessVerifiableCredential } from '../types/businessVCtype';
 import { personVerifiableCredential } from '../types/personVCType';
-
+/**
+ * IStateAgentController is an interface for the functions within the StateAgentController class.
+ */
 export interface IStateAgentController {
     createPersonCredential(issuerDid: string, credentialSubjectData: personVerifiableCredential['credentialSubject']): Promise<VerifiableCredential | Error>
     
--- src/interfaces/symfoniControllerInterface.ts
+++ src/interfaces/symfoniControllerInterface.ts
@@ -2,8 +2,13 @@ import { VerifiableCredential } from '@veramo/core';
 import { employmentVC } from '../types/employmentVCType';
 import { terminationVC } from '../types/terminationVCType';
 
+/**
+ * ISymfoniAgentController is an interface for the functions within the SymfoniAgentController class.
+ */
 export interface ISymfoniAgentController {
     createEmploymentCredential(issuer: string, credentialData: employmentVC['credentialSubject']): Promise<VerifiableCredential | Error>
     
     createTerminationCredential(issuer: string, credentialData: terminationVC['credentialSubject']): Promise<VerifiableCredential | Error>
+
+    isQualifiedForContractVCs(presentationToken: string): Promise<boolean | Error>
 }
