fb7849ecd22c849d72d4d6c22fb11f8e15cf173f
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-05-11 13:32:56 +0200

    added a function that can compute months between two dates

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -14,3 +14,18 @@ export function computeYearsBetweenDates(date1: string, date2: string): number {
     
 	return Math.abs(yearDiff);
 }
+
+/**
+ * computeMonthsBetweenDates computes the month difference between two dates.
+ * @param date1 the first date as a string
+ * @param date2 the second date as a string
+ * @returns the absolute value of the month difference.
+ */
+export function computeMonthsBetweenDates(date1: string, date2: string): number{
+	const momentDate1 = moment(date1);
+	const momentDate2 = moment(date2);
+
+	const monthDiff = momentDate1.diff(momentDate2, 'months');
+
+	return Math.abs(monthDiff);
+}
